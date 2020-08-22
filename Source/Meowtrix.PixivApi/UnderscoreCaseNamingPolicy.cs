25eed1ba30dd1cd1453283d0c72ff8d050c20223
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-08-22 15:49:53 +0800

    Setup multi target and add json naming policy.

---
+++ Source/Meowtrix.PixivApi/UnderscoreCaseNamingPolicy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+
+namespace Meowtrix.PixivApi
+{
+    internal class UnderscoreCaseNamingPolicy : JsonNamingPolicy
+    {
+        public override string ConvertName(string name)
+        {
+            var sb = new StringBuilder();
+            sb.Append(char.ToLowerInvariant(name[0]));
+
+            foreach (char ch in name.AsSpan(1))
+            {
+                if (char.IsUpper(ch))
+                {
+                    sb.Append('_');
+                    sb.Append(char.ToLowerInvariant(ch));
+                }
+                else
+                {
+                    sb.Append(ch);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
