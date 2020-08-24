5392c2c63cfeb0aa93a8ef5e26c4b54dc57a294e
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-08-24 21:12:47 +0800

    Add login in manual test.

--- Source/Meowtrix.PixivApi.ManualTest/Program.cs
+++ Source/Meowtrix.PixivApi.ManualTest/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Meowtrix.PixivApi.Json;
+
+#pragma warning disable CA2007
 
 namespace Meowtrix.PixivApi.ManualTest
 {
     internal class Program
     {
-        public static void Main()
+        public static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please use debugger to inspect remote returned data.");
+            Console.Write("Proxy:");
+            string? proxy = Console.ReadLine();
+
+            using var client = string.IsNullOrWhiteSpace(proxy)
+                ? new PixivApiClient()
+                : new PixivApiClient(new WebProxy($"http://{proxy}"));
+
+            Console.Write("Saved access token:");
+            string? authToken = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(authToken))
+            {
+                Console.Write("Saved refresh token:");
+                string? refreshToken = Console.ReadLine();
+
+                Response response;
+                if (!string.IsNullOrWhiteSpace(refreshToken))
+                {
+                    response = (await client.AuthAsync(refreshToken)).authResponse.Response;
+                }
+                else
+                {
+                    Console.Write("Username:");
+                    string username = Console.ReadLine()!;
+                    Console.Write("Password:");
+                    string password = Console.ReadLine()!;
+                    response = (await client.AuthAsync(username, password)).authResponse.Response;
+                }
+                authToken = response.AccessToken;
+                Console.WriteLine($"Access token: {response.AccessToken}");
+                Console.WriteLine($"Refresh token: {response.RefreshToken}");
+            }
         }
     }
 }
