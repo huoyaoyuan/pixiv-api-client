c0e3e09c3da2380395184c4936070a78e0febcdb
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-08-30 16:18:00 +0800

    Remove GenerateNullTestAttribute.

--- Source/Meowtrix.PixivApi/GenerateNullTestAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Meowtrix
-{
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
-    internal class GenerateNullTestAttribute : Attribute
-    {
-    }
-}
