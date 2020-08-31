23bd74bfeb8e3b7ad521ebedf86373c129541509
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-08-31 22:02:48 +0800

    Add locked access token refresh.

--- Source/Meowtrix.PixivApi/PixivClient.cs
+++ Source/Meowtrix.PixivApi/PixivClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Meowtrix.PixivApi
 {
@@ -48,7 +50,99 @@ namespace Meowtrix.PixivApi
             _api = api;
         }
 
-        public void Dispose() => _api.Dispose();
+        public void Dispose()
+        {
+            _api.Dispose();
+            _authLock.Dispose();
+        }
+        #endregion
+
+        #region Lock and token refresh
+        private readonly ReaderWriterLockSlim _authLock = new ReaderWriterLockSlim();
+        private DateTimeOffset _authValidateUntil;
+
+        private string? _accessToken;
+        private string? _refreshToken;
+
+#if NET5_0
+        [System.Diagnostics.CodeAnalysis.MemberNotNullWhen(true, nameof(_accessToken), nameof(_refreshToken))]
+#endif
+        public bool IsLogin => _accessToken is not null && _refreshToken is not null;
+
+        public async Task LoginAsync(string username, string password)
+        {
+            try
+            {
+                _authLock.EnterWriteLock();
+
+                var (time, response) = await _api.AuthAsync(username, password).ConfigureAwait(false);
+                _accessToken = response.AccessToken;
+                _refreshToken = response.RefreshToken;
+                _authValidateUntil = time.AddSeconds(response.ExpiresIn);
+            }
+            finally
+            {
+                _authLock.ExitWriteLock();
+            }
+        }
+
+        public async Task LoginAsync(string refreshToken)
+        {
+            try
+            {
+                _authLock.EnterWriteLock();
+
+                var (time, response) = await _api.AuthAsync(refreshToken).ConfigureAwait(false);
+                _accessToken = response.AccessToken;
+                _refreshToken = response.RefreshToken;
+                _authValidateUntil = time.AddSeconds(response.ExpiresIn);
+            }
+            finally
+            {
+                _authLock.ExitWriteLock();
+            }
+        }
+
+        private async ValueTask<string> CheckValidAccessToken(int epsilonTimeSeconds = 60)
+        {
+            if (!IsLogin)
+            {
+                throw new InvalidOperationException("The client isn't logged in.");
+            }
+
+            try
+            {
+                _authLock.EnterUpgradeableReadLock();
+
+                if ((_authValidateUntil - DateTimeOffset.Now).TotalSeconds < epsilonTimeSeconds)
+                {
+                    try
+                    {
+                        _authLock.EnterWriteLock();
+
+                        if ((_authValidateUntil - DateTimeOffset.Now).TotalSeconds < epsilonTimeSeconds)
+                        {
+                            var (time, response) = await _api.AuthAsync(_refreshToken).ConfigureAwait(false);
+                            _accessToken = response.AccessToken;
+                            _refreshToken = response.RefreshToken;
+                            _authValidateUntil = time.AddSeconds(response.ExpiresIn);
+
+                            return _accessToken;
+                        }
+                    }
+                    finally
+                    {
+                        _authLock.ExitWriteLock();
+                    }
+                }
+            }
+            finally
+            {
+                _authLock.ExitUpgradeableReadLock();
+            }
+
+            return _accessToken;
+        }
         #endregion
     }
 }
