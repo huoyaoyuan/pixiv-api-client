ac2ec9b570efc94a5f354ec83ae55ba1b1e5f706
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-11-28 18:46:58 +0800

    Bookmark count in illust filter options should be nullable.

--- Source/Meowtrix.PixivApi/Models/IllustFilterOptions.cs
+++ Source/Meowtrix.PixivApi/Models/IllustFilterOptions.cs
@@ -5,8 +5,8 @@ namespace Meowtrix.PixivApi.Models
     public record IllustFilterOptions
     {
         public IllustSortMode SortMode { get; init; } = IllustSortMode.Latest;
-        public int MaxBookmarkCount { get; init; }
-        public int MinBookmarkCount { get; init; }
+        public int? MaxBookmarkCount { get; init; }
+        public int? MinBookmarkCount { get; init; }
         public DateTime? StartDate { get; init; }
         public DateTime? EndDate { get; init; }
     }
