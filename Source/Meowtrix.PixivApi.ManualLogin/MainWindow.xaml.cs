a3b9e210ab801acb53c2c965def142767c909dee
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-11-09 15:08:13 +0800

    Sample code for login

--- Source/Meowtrix.PixivApi.ManualLogin/MainWindow.xaml.cs
+++ Source/Meowtrix.PixivApi.ManualLogin/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace Meowtrix.PixivApi.ManualLogin
 {
@@ -7,9 +9,29 @@ namespace Meowtrix.PixivApi.ManualLogin
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private readonly PixivClient _client = new();
+
+        public MainWindow() => InitializeComponent();
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            InitializeComponent();
+            string refreshToken = await _client.LoginAsync(uri =>
+            {
+                var tcs = new TaskCompletionSource<Uri>();
+                WebView.Source = new(uri);
+                WebView.NavigationStarting += (s, e) =>
+                {
+                    if (e.Uri.StartsWith("pixiv:", StringComparison.Ordinal))
+                    {
+                        e.Cancel = true;
+                        tcs.SetResult(new(e.Uri));
+                    }
+                };
+
+                return tcs.Task;
+            }).ConfigureAwait(true);
+
+            Text.Text = $"Your refresh token is: \n\n{refreshToken}\n\nDO NOT share it -- it's valid for a long term.";
         }
     }
 }
