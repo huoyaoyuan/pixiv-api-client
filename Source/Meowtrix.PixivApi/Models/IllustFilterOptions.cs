3189c83abaf22c8dd5b9ff0edaac10938d943be1
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-11-11 03:54:57 +0800

    Use DateOnly in client in net6

--- Source/Meowtrix.PixivApi/Models/IllustFilterOptions.cs
+++ Source/Meowtrix.PixivApi/Models/IllustFilterOptions.cs
@@ -7,7 +7,25 @@ namespace Meowtrix.PixivApi.Models
         public IllustSortMode SortMode { get; init; } = IllustSortMode.Latest;
         public int? MaxBookmarkCount { get; init; }
         public int? MinBookmarkCount { get; init; }
+
+#if NET6_0_OR_GREATER
+        public DateOnly? StartDateOnly { get; init; }
+        public DateOnly? EndDateOnly { get; init; }
+
+        public DateTime? StartDate
+        {
+            get => StartDateOnly?.ToDateTime(default, DateTimeKind.Unspecified);
+            init => StartDateOnly = value is DateTime d ? DateOnly.FromDateTime(d) : null;
+        }
+
+        public DateTime? EndDate
+        {
+            get => EndDateOnly?.ToDateTime(default, DateTimeKind.Unspecified);
+            init => EndDateOnly = value is DateTime d ? DateOnly.FromDateTime(d) : null;
+        }
+#else
         public DateTime? StartDate { get; init; }
         public DateTime? EndDate { get; init; }
+#endif
     }
 }
--- Source/Meowtrix.PixivApi/PixivClient.cs
+++ Source/Meowtrix.PixivApi/PixivClient.cs
@@ -355,6 +355,18 @@ namespace Meowtrix.PixivApi
             IllustFilterOptions? options = null,
             CancellationToken cancellation = default)
         {
+#if NET6_0_OR_GREATER
+            return ToIllustAsyncEnumerable(async (auth, c)
+                => await Api.SearchIllustsAsync(
+                    authToken: auth, word: word,
+                    searchTarget: searchTarget,
+                    sort: options?.SortMode ?? IllustSortMode.Latest,
+                    maxBookmarkCount: options?.MaxBookmarkCount,
+                    minBookmarkCount: options?.MinBookmarkCount,
+                    startDate: options?.StartDateOnly,
+                    endDate: options?.EndDateOnly,
+                    cancellation: c).ConfigureAwait(false), cancellation);
+#else
             return ToIllustAsyncEnumerable(async (auth, c)
                 => await Api.SearchIllustsAsync(
                     authToken: auth, word: word,
@@ -365,6 +377,7 @@ namespace Meowtrix.PixivApi
                     startDate: options?.StartDate,
                     endDate: options?.EndDate,
                     cancellation: c).ConfigureAwait(false), cancellation);
+#endif
         }
 
         public IAsyncEnumerable<Illust> GetIllustRankingAsync(
@@ -378,6 +391,20 @@ namespace Meowtrix.PixivApi
                     date: date,
                     cancellation: cancellation).ConfigureAwait(false), cancellation);
         }
+
+#if NET6_0_OR_GREATER
+        public IAsyncEnumerable<Illust> GetIllustRankingAsync(
+            IllustRankingMode rankingMode = IllustRankingMode.Day,
+            DateOnly? date = null,
+            CancellationToken cancellation = default)
+        {
+            return ToIllustAsyncEnumerable(async (auth, c)
+                => await Api.GetIllustRankingAsync(
+                    authToken: auth, mode: rankingMode,
+                    date: date,
+                    cancellation: cancellation).ConfigureAwait(false), cancellation);
+        }
+#endif
 
         public async Task FollowUserAsync(
             UserInfo userInfo,
