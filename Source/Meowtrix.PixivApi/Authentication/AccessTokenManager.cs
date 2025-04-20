908f351722a1962586142cbdb9175e99a8f81e63
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2025-04-20 19:33:53 +0800

    Access token management

---
+++ Source/Meowtrix.PixivApi/Authentication/AccessTokenManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Meowtrix.PixivApi.Authentication;
+
+public class AccessTokenManager(
+    PixivAuthenticationResult? initialAuthResult,
+    TimeProvider? timeProvider = null)
+{
+    private readonly TimeProvider _timeProvider = timeProvider ?? TimeProvider.System;
+
+    private PixivAuthenticationResult? _authResult = initialAuthResult;
+
+    public TimeSpan RefreshThreshold { get; set; } = TimeSpan.FromMinutes(10);
+
+    public void Authenticate(PixivAuthenticationResult authResult) => _authResult = authResult;
+
+    public async ValueTask<string> GetAccessTokenAsync(HttpMessageHandler handler, CancellationToken cancellationToken = default)
+    {
+        var authResult = _authResult ?? throw new InvalidOperationException("No authentication has been performed.");
+
+        if (authResult.ValidUntil - _timeProvider.GetUtcNow() > RefreshThreshold)
+            return authResult.AccessToken;
+
+        _authResult = authResult = await PixivAuthentication.AuthWithRefreshTokenAsync(
+            new HttpMessageInvoker(handler, disposeHandler: false), authResult.RefreshToken, _timeProvider, cancellationToken)
+            .ConfigureAwait(false);
+        return authResult.AccessToken;
+    }
+}
---
+++ Source/Meowtrix.PixivApi/Authentication/PixivAccessTokenHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace Meowtrix.PixivApi.Authentication;
+
+internal class PixivAccessTokenHandler(HttpMessageHandler innerHandler, AccessTokenManager tokenManager) : DelegatingHandler(innerHandler)
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        request.SetBearerToken(await tokenManager.GetAccessTokenAsync(InnerHandler!, cancellationToken).ConfigureAwait(false));
+        return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+    }
+
+#if NET
+    protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        request.SetBearerToken(tokenManager.GetAccessTokenAsync(InnerHandler!, cancellationToken).Preserve().Result);
+        return base.Send(request, cancellationToken);
+    }
+#endif
+}
