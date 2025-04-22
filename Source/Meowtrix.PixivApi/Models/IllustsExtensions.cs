33c3a7cadb256951b8f666823d838a836d5709ed
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2025-04-22 21:24:32 +0800

    Use more async linq

--- Source/Meowtrix.PixivApi/Models/IllustsExtensions.cs
+++ Source/Meowtrix.PixivApi/Models/IllustsExtensions.cs
@@ -1,28 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Meowtrix.PixivApi.Models
 {
     public static class IllustsExtensions
     {
-        public static async IAsyncEnumerable<Illust> R18(this IAsyncEnumerable<Illust> source)
-        {
-            await foreach (var i in source.ConfigureAwait(false))
-            {
-                if (i.IsR18)
-                    yield return i;
-            }
-        }
+        public static IAsyncEnumerable<Illust> R18(this IAsyncEnumerable<Illust> source)
+            => source.Where(x => x.IsR18);
 
-        public static async IAsyncEnumerable<Illust> AllAge(this IAsyncEnumerable<Illust> source)
-        {
-            await foreach (var i in source.ConfigureAwait(false))
-            {
-                if (!i.IsR18)
-                    yield return i;
-            }
-        }
+        public static IAsyncEnumerable<Illust> AllAge(this IAsyncEnumerable<Illust> source)
+            => source.Where(x => !x.IsR18);
 
         public static IAsyncEnumerable<Illust> Age(this IAsyncEnumerable<Illust> source, AgeRestriction age)
             => age switch
@@ -32,22 +19,10 @@ namespace Meowtrix.PixivApi.Models
                 _ => source
             };
 
-        public static async IAsyncEnumerable<Illust> WithTag(this IAsyncEnumerable<Illust> source, string tag)
-        {
-            await foreach (var i in source.ConfigureAwait(false))
-            {
-                if (i.Tags.Any(t => t.Name == tag))
-                    yield return i;
-            }
-        }
+        public static IAsyncEnumerable<Illust> WithTag(this IAsyncEnumerable<Illust> source, string tag)
+            => source.Where(x => x.Tags.Any(t => t.Name == tag));
 
-        public static async IAsyncEnumerable<Illust> WithoutTag(this IAsyncEnumerable<Illust> source, string tag)
-        {
-            await foreach (var i in source.ConfigureAwait(false))
-            {
-                if (!i.Tags.Any(t => t.Name == tag))
-                    yield return i;
-            }
-        }
+        public static IAsyncEnumerable<Illust> WithoutTag(this IAsyncEnumerable<Illust> source, string tag)
+            => source.Where(x => !x.Tags.Any(t => t.Name == tag));
     }
 }
